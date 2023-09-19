e10f0cb6891e2ed78652892e2eca20475c9539c9
NGUYEN-QUOC-TUAN <fakerphong@gmail.com>
2023-09-19 22:31:34 +0700

    first commit

---
+++ src/main/java/com/example/web_ban_link_kien_pc/WebBanLinkKienPcApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_link_kien_pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanLinkKienPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBanLinkKienPcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_ban_link_kien_pc/WebBanLinkKienPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_link_kien_pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanLinkKienPcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
