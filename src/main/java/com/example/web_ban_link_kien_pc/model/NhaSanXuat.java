d54f021b6ee532c683fe67c916c85e1625fe05c2
Đoàn Tiến Triển <triendoan4432@gmail.com>
2023-09-26 22:14:59 +0700

    Push CRUD TK NSX

--- src/main/java/com/example/web_ban_link_kien_pc/model/NhaSanXuat.java
+++ src/main/java/com/example/web_ban_link_kien_pc/model/NhaSanXuat.java
@@ -1,4 +1,35 @@
 package com.example.web_ban_link_kien_pc.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "nhasanxuat")
 public class NhaSanXuat {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
+    private String id;
+    @Column(name = "ma", unique = true, nullable = false, length = 36)
+    private String ma;
+    @Column(name = "ten", columnDefinition = "NVARCHAR(70)")
+    private String ten;
+
 }
