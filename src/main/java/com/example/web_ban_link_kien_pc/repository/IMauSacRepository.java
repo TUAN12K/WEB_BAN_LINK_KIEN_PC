f370cc731e8bfddb7508a166632f0bf6a4341d32
do hong kien <dohongkien2003@gmail.com>
2023-10-03 21:27:01 +0700

    add interface

--- src/main/java/com/example/web_ban_link_kien_pc/repository/IMauSacRepository.java
+++ src/main/java/com/example/web_ban_link_kien_pc/repository/IMauSacRepository.java
@@ -4,5 +4,4 @@ import com.example.web_ban_link_kien_pc.model.MauSac;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IMauSacRepository extends JpaRepository<MauSac, String> {
-
 }
--- src/main/java/com/example/web_ban_link_kien_pc/repository/ISanPhamRepository.java
+++ src/main/java/com/example/web_ban_link_kien_pc/repository/ISanPhamRepository.java
@@ -4,5 +4,4 @@ import com.example.web_ban_link_kien_pc.model.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ISanPhamRepository extends JpaRepository<SanPham, String> {
-
 }
