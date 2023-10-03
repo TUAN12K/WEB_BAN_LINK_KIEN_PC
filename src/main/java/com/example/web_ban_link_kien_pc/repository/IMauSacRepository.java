caad65283dfbc698ca6d9606fba87fac8becc403
do hong kien <dohongkien2003@gmail.com>
2023-10-03 21:24:30 +0700

    update interface

--- src/main/java/com/example/web_ban_link_kien_pc/repository/IMauSacRepository.java
+++ src/main/java/com/example/web_ban_link_kien_pc/repository/IMauSacRepository.java
@@ -4,4 +4,5 @@ import com.example.web_ban_link_kien_pc.model.MauSac;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IMauSacRepository extends JpaRepository<MauSac, String> {
+
 }
--- src/main/java/com/example/web_ban_link_kien_pc/repository/ISanPhamRepository.java
+++ src/main/java/com/example/web_ban_link_kien_pc/repository/ISanPhamRepository.java
@@ -4,4 +4,5 @@ import com.example.web_ban_link_kien_pc.model.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ISanPhamRepository extends JpaRepository<SanPham, String> {
+
 }
